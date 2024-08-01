9e2ba71f555b7c09d4e26e167cdd9121772514d5
Trần Trọng Nhân <93432020+nhantran5545@users.noreply.github.com>
2024-08-01 14:23:01 +0700

    Add project files.

---
+++ BlazerSignalRChatApp/BlazerSignalRChatApp.Client/ChatServices/ChatService.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazerSignalRChatApp.Client.ChatServices
+{
+    public class ChatService
+    {
+        public Action? InvokeChatDisplay { get; set; }
+
+    }
+}
---
+++ BlazerSignalRChatApp/BlazerSignalRChatApp/ChatHubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using BlazerSingalRChatApp.DataAccess;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazerSignalRChatApp.ChatHubs
+{
+    public class ChatHub : Hub 
+    {
+        public async Task SendMessage(Chat chat)
+            => await Clients.All.SendAsync("MessageReceived", chat);
+    }
+}
